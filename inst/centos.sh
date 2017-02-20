72e47c178b6e79c129f5465f687c344508a35334
Lan Tian <xuyh0120@outlook.com>
2017-02-20 22:30:59 +0800

    Customization

--- inst/centos.sh
+++ inst/centos.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-yum install gcc make -y
-yum install wget -y
-cd /tmp
-wget http://humdi.net/vnstat/vnstat-1.12.tar.gz
-tar xzf vnstat-1.12.tar.gz
-cd /tmp/vnstat-1.12
-
-make 
-make install
-cp /tmp/vnstat-1.12/examples/init.d/redhat/vnstat /etc/init.d/vnstat
-chmod +x /etc/init.d/vnstat
-/etc/init.d/vnstat start
-cd /etc/init.d/
-chkconfig --add vnstat
-chkconfig vnstat on
-
 cd /usr/bin/
-wget https://git.enjen.net/Munzy/custom_motd/raw/master/custom_motd
+wget https://github.com/xddxdd/custom_motd/raw/master/custom_motd
 chmod +x custom_motd
 
 echo 'custom_motd' >> /etc/profile
--- inst/deb.sh
+++ inst/deb.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 apt-get update 
-apt-get install ca-certificates -y
-apt-get install vnstat -y
-apt-get install wget -y
-
+apt-get install ca-certificates wget -y
 
 cd /usr/bin/
-wget https://git.enjen.net/Munzy/custom_motd/raw/master/custom_motd
+wget https://github.com/xddxdd/custom_motd/raw/master/custom_motd
 chmod +x custom_motd
 
 echo 'custom_motd' >> /etc/profile
